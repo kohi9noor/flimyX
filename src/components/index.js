b3badd3258da08ca7bf6e0a4d829b5a6e6c67e16
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2024-01-12 11:45:33 +0200

    create skeleton

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actors } from "./actors/Actors";
+export { default as MovieInfo } from "./movieInformation/MovieInfo";
+export { default as Navbar } from "./NavBar/Navbar";
+export { default as Movies } from "./movies/movies";
+export { default as Profile } from "./Profile/Profile";
---
+++ src/components/movieInformation/MovieInfo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MovieInfo = () => {
+  console.log("Movie Infrmation");
+  return <div>MovieInfo</div>;
+};
+
+export default MovieInfo;
