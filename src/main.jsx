9eb5d009652d13e6629e69506487c85c2ca6ab3d
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2024-02-11 11:41:14 +0200

    added: [web analyzer]

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 
+import { inject } from "@vercel/analytics";
+
+inject();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
