82506ec61cf8519cde49ec16e740b89b067bd7d4
kohi9noor <116247355+kohi9noor@users.noreply.github.com>
2024-02-11 07:48:32 +0200

    fix: [cast profile bug]

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,14 +1,13 @@
+import { useParams } from "react-router-dom";
 import "./style.scss";
 
-import DetailsBanner from "./detailsBanner/Details.Banner";
-import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/Details.Banner";
+import Cast from "../../components/cast/Cast";
 
 const Details = () => {
   const { mediaType, id } = useParams();
-
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-
   const { data: credits, loading: creditsLoading } = useFetch(
     `/${mediaType}/${id}/credits`
   );
@@ -16,6 +15,7 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   );
 };
